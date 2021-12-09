bc48033f46c2f671279a0f6f740309c23fffc0ec
Hang Su <jsu6898@gmail.com>
2021-12-08 23:48:35 -0800

    creates show appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const anyAppointment = () => {
-    return props.time ? `Appointment at ${props.time}` : "No Appointments";
-  };
-  return <article className="appointment">{anyAppointment()}</article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
